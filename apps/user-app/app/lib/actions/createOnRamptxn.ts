b929d05ae96f8845dd899f5f5639736dd84d7e6d
Lakhan lal <lakhan191136@dei.ac.in>
2024-05-24 23:57:31 +0530

    fix onRamp issue

---
+++ apps/user-app/app/lib/actions/createOnRamptxn.ts
@@ -0,0 +1,34 @@
+"use server";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth";
+import prisma from "@repo/db/client";
+
+export async function createOnRampTransaction(
+  amount: number,
+  provider: string
+) {
+  const session = await getServerSession(authOptions);
+  const userId = session.user.id;
+  const token = Math.random().toString();
+
+  if (!userId) {
+    return {
+      message: "User not logged in!!",
+    };
+  }
+
+  await prisma.onRampTransaction.create({
+    data: {
+      userId: Number(userId),
+      amount,
+      provider,
+      status: "Processing",
+      startTime: new Date(),
+      token,
+    },
+  });
+
+  return {
+    message: "On ramp transaction added",
+  };
+}
