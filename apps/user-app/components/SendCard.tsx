213e3554d4a82eb89f8a7d8f8476d3a46a0cb53e
Lakhan lal <lakhan191136@dei.ac.in>
2024-05-27 11:40:43 +0530

    update sendCard

--- apps/user-app/components/SendCard.tsx
+++ apps/user-app/components/SendCard.tsx
@@ -5,12 +5,14 @@ import { Center } from "@repo/ui/center";
 import { TextInput } from "@repo/ui/textinput";
 import { useState } from "react";
 import { p2pTransfer } from "../app/lib/actions/p2pTransfer";
+import { useRouter } from "next/navigation";
 
 export function SendCard() {
   const [number, setNumber] = useState("");
   const [amount, setAmount] = useState("");
+  const router = useRouter();
   return (
-    <div className="h-[90vh]">
+    <div className="mt-4">
       <Center>
         <Card title="Send">
           <div className="min-w-72 pt-2">
@@ -31,7 +33,16 @@ export function SendCard() {
             <div className="pt-4 flex justify-center">
               <Button
                 onClick={async () => {
-                  await p2pTransfer(number, Number(amount) * 100);
+                  try {
+                    const res = await p2pTransfer(number, Number(amount) * 100);
+                    if (res) {
+                      alert(res.message);
+                    } else {
+                      router.push("/transfer");
+                    }
+                  } catch (error) {
+                    alert("Insufficient Fund");
+                  }
                 }}
               >
                 Send
